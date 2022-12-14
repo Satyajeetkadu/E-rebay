216ef0656987f7ae52c9326d2f81316477634967
Nilay Gaitonde <68230775+NilayGaitonde@users.noreply.github.com>
2022-12-14 14:11:24 +0530

    Delinqunecy done

--- main.py
+++ main.py
@@ -170,9 +170,24 @@ def create_loan(text):
         delinquecy = False
         accountNoIndex = text.find("Acct # :")
         text = text[accountNoIndex+1:]
-        # print(accountNoIndex)
-        # delinquencies
-        # delinquecy_count =check_delinquencies(text)
+
+        # DELINQUENCIES
+
+        delinquenciesIndex = get_index(text.find("Suit Filed Status:"),"Suit Filed Status:")
+        if(text[delinquenciesIndex+1].strip() == "H"):
+            delinquencyString = text[(delinquenciesIndex+len("HistoryAccount Status:Asset Classification:Suit Filed Status:")+4):(text.find("Acct # :"))]
+            # print(delinquencyString)
+            # Get all index of string "-22" is present in the delinquency string
+            index = [m.start() for m in re.finditer('-22', delinquencyString)]
+            if(len(index)>0):
+                try:
+                    six_months = index[0:7]
+                except IndexError:
+                    six_months = index
+                delinquencyString = delinquencyString[six_months[0]:(six_months[-1]+2)]
+                delinquenciesCount = (delinquencyString.count("+")+delinquencyString.count("CLSD")+delinquencyString.count("WOF")+delinquencyString.count("RCV"))
+            else:
+                delinquenciesCount = 0
         openIndex = get_index(text.find('Open: '),'Open: ')
         openValue = text[openIndex:(text.find("Date Reported: "))].strip()
 
@@ -261,7 +276,7 @@ def create_loan(text):
         completeDF['EMI'].append(int(EMIValue))
         completeDF['Paid Principle'].append(int(sanction_credit-Balance))
         completeDF['open'].append(openValue)
-        completeDF['Delinquencies'].append(0)
+        completeDF['Delinquencies'].append(delinquenciesCount)
         completeDF['date_opened'].append(date_object)
     return completeDF
 
